c411d2a03b7d590fd4dfd25225b9c407c0132ec0
David <s1155003185@gmail.com>
2023-08-01 23:21:59 +0800

    [ProjectUpdater] Pull Project to latest

--- include/Service/vpg_file_update_service.hpp
+++ include/Service/vpg_file_update_service.hpp
@@ -12,12 +12,17 @@ class VPGFileUpdateService
 {
     private:
         static std::wstring _DefaultFolder(LogProperty &logProperty);
-        static std::wstring _DownloadVCCResource(LogProperty &logProperty, std::wstring url, std::wstring branch, std::wstring directory, bool forceUpdate);
+        static std::wstring _GetProjectName(VPGDllType dllType);
+        static std::wstring _GetProjectName(VPGInterfaceType interfaceType);
 
+        static std::wstring _DownloadVCCResource(LogProperty &logProperty, std::wstring url, std::wstring branch, std::wstring directory);
+        static std::wstring _UpdateVCCResource(LogProperty &logProperty, std::wstring workspace);
     public:
         VPGFileUpdateService() {}
         ~VPGFileUpdateService() {}
 
-        static std::wstring DownloadVCCResource(LogProperty &logProperty, VPGDllType dllType, std::wstring directory, bool forceUpdate);
-        static std::wstring DownloadVCCResource(LogProperty &logProperty, VPGInterfaceType interfaceType, std::wstring directory, bool forceUpdate);
+        static std::wstring DownloadVCCResource(LogProperty &logProperty, VPGDllType dllType, std::wstring directory);
+        static std::wstring DownloadVCCResource(LogProperty &logProperty, VPGInterfaceType interfaceType, std::wstring directory);
+        static std::wstring UpdateVCCResource(LogProperty &logProperty, VPGDllType dllType, std::wstring directory);
+        static std::wstring UpdateVCCResource(LogProperty &logProperty, VPGInterfaceType interfaceType, std::wstring directory);
 };
