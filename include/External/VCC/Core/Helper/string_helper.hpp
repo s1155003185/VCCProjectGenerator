b113777bf0a79b23d93abf0fdec2356b05b9006b
David <s1155003185@gmail.com>
2023-08-24 22:16:49 +0800

    [String] File filter to Regex

--- include/External/VCC/Core/Helper/string_helper.hpp
+++ include/External/VCC/Core/Helper/string_helper.hpp
@@ -1,23 +1,37 @@
 #pragma once
 #include <string>
+#include <regex>
 
 namespace vcc
 {
     constexpr auto NL = L"\n";
 
-	std::wstring str2wstr(const std::string& str);
+	enum class EscapeStringType
+	{
+		Regex
+	};
 
+	std::wstring str2wstr(const std::string& str);
 	std::string wstr2str(const std::wstring &wstr);
 
 	std::string PadLeft(const std::string str, size_t length, char c);
-
 	std::string PadRight(const std::string str, size_t length, char c);
 
-	void ltrim(std::string &str);
-	void rtrim(std::string &str);
-	void trim(std::string &str);
+	void LTrim(std::string &str);
+	void RTrim(std::string &str);
+	void Trim(std::string &str);
 
-	void ltrim(std::wstring &str);
-	void rtrim(std::wstring &str);
-	void trim(std::wstring &str);
+	void LTrim(std::wstring &str);
+	void RTrim(std::wstring &str);
+	void Trim(std::wstring &str);
+
+	void Replace(std::wstring &str, const std::wstring& from, const std::wstring &to);
+	void ReplaceAll(std::wstring &str, const std::wstring& from, const std::wstring &to);
+
+	// regex
+	std::vector<wchar_t> GetSpecialCharacters(const EscapeStringType &type);
+	wchar_t GetEscapeCharacter(const EscapeStringType &type);
+	std::wstring GetEscapeString(const EscapeStringType &type, const std::wstring &str);
+	std::wstring GetRegexFromFileFilter(const std::wstring &fileFilter);
+
 }
--- src/External/VCC/Core/Helper/string_helper.cpp
+++ src/External/VCC/Core/Helper/string_helper.cpp
@@ -1,8 +1,13 @@
 #include "string_helper.hpp"
 
+#include <algorithm>
 #include <math.h>
 #include <memory>
 #include <string>
+#include <vector>
+
+#include "exception.hpp"
+#include "exception_macro.hpp"
 
 namespace vcc
 {
@@ -42,43 +47,119 @@ namespace vcc
 		return str + std::string(length - std::min(length, str.length()), c);
 	}
 
-	void ltrim(std::string &str)
+	void LTrim(std::string &str)
 	{
 		str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](char c) {
 			return !std::isspace(c);
 		}));
 	}
 
-	void rtrim(std::string &str)
+	void RTrim(std::string &str)
 	{
 		str.erase(std::find_if(str.rbegin(), str.rend(), [](char c) {
 			return !std::isspace(c);
 		}).base(), str.end());
 	}
 
-	void trim(std::string &str)
+	void Trim(std::string &str)
 	{
-		ltrim(str);
-		rtrim(str);
+		LTrim(str);
+		RTrim(str);
 	}
 
-	void ltrim(std::wstring &str)
+	void LTrim(std::wstring &str)
 	{
 		str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](wchar_t c) {
 			return !std::iswspace(c);
 		}));
 	}
 
-	void rtrim(std::wstring &str)
+	void RTrim(std::wstring &str)
 	{
 		str.erase(std::find_if(str.rbegin(), str.rend(), [](wchar_t c) {
 			return !std::iswspace(c);
 		}).base(), str.end());
 	}
 
-	void trim(std::wstring &str)
+	void Trim(std::wstring &str)
 	{
-		ltrim(str);
-		rtrim(str);
+		LTrim(str);
+		RTrim(str);
 	}
+
+    void Replace(std::wstring &str, const std::wstring& from, const std::wstring &to)
+    {
+        str.replace(str.find(from), from.length(), to);
+    }
+
+	void ReplaceAll(std::wstring &str, const std::wstring& from, const std::wstring &to)
+    {
+        size_t startPos = 0;
+        size_t foundPos;
+        while ((foundPos = str.find(from, startPos)) != std::wstring::npos) {
+            str.replace(foundPos, from.length(), to);
+            startPos = foundPos + to.length();
+        }
+    }
+
+    std::vector<wchar_t> GetSpecialCharacters(const EscapeStringType &type)
+    {
+        std::vector<wchar_t> result;
+        switch (type)
+        {
+            case EscapeStringType::Regex:
+                result = { L'\\', L'^', L'$', L'.', L'|', L'?', L'*', L'+', L'(', L')', L'[', L']', L'{', L'}' };
+                break;
+            default:
+                assert(false);
+        }
+        return result;
+    }
+
+    wchar_t GetEscapeCharacter(const EscapeStringType &type)
+    {
+        switch (type)
+        {
+            case EscapeStringType::Regex:
+                return L'\\';
+            default:
+                assert(false);
+        }
+        return '\0';
+    }
+
+	std::wstring GetEscapeString(const EscapeStringType &type, const std::wstring &str)
+    {
+        std::wstring result = L"";
+        try 
+        {
+            std::vector<wchar_t> specChars = GetSpecialCharacters(type);
+            std::wstring escapChar = std::wstring(1, GetEscapeCharacter(type));
+            for (auto c : str) {
+                if (std::count(specChars.begin(), specChars.end(), c)) {
+                    result += escapChar + std::wstring(1, c);
+                } else {
+                    result += std::wstring(1, c);
+                }
+            }
+        }
+        catch(std::exception& ex)
+        {
+            THROW_EXCEPTION(ex);
+        }
+        return result;
+    }
+
+
+    std::wstring GetRegexFromFileFilter(const std::wstring &fileFilter)
+    {
+        std::wstring patten = GetEscapeString(EscapeStringType::Regex, fileFilter);
+
+        // Excape *
+        ReplaceAll(patten, L"\\*", L".*");
+
+        // Excape ?
+        ReplaceAll(patten, L"\\?", L".");
+        return patten;
+    }
 }
