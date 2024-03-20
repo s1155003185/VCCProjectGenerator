29aaf51f66e41e2dc4f3304c8baa658f39fe1ee0
David <s1155003185@gmail.com>
2024-03-20 23:13:55 +0800

    json add unit test case

--- unittest/External/VCC/Core/DocumentBuilder/json_builder_test.cpp
+++ unittest/External/VCC/Core/DocumentBuilder/json_builder_test.cpp
@@ -7,7 +7,38 @@
 
 using namespace vcc;
 
-TEST(JsonBuilderTest, Normal)
+TEST(JsonBuilderTest, String)
 {
+    std::wstring str = L"{\"name\":\"John\"}";
+    std::unique_ptr<JsonBuilder> builder = std::make_unique<JsonBuilder>();
+}
+
+TEST(JsonBuilderTest, Number)
+{
+    std::wstring str = L"{\"age\":11}";
+    std::unique_ptr<JsonBuilder> builder = std::make_unique<JsonBuilder>();
+}
+
+TEST(JsonBuilderTest, Object)
+{
+    std::wstring str = L"{\"Name\":{ \"firstName\":\"A\", \"lastName\":\"B\" }}";
+    std::unique_ptr<JsonBuilder> builder = std::make_unique<JsonBuilder>();
+}
+
+TEST(JsonBuilderTest, Array)
+{
+    std::wstring str = L"{\"employees\":[{ \"firstName\":\"A\", \"lastName\":\"B\" }, { \"firstName\":\"C\", \"lastName\":\"D\" }]}";
+    std::unique_ptr<JsonBuilder> builder = std::make_unique<JsonBuilder>();
+}
+
+TEST(JsonBuilderTest, Null)
+{
+    std::wstring str = L"{\"Name\":null}";
+    std::unique_ptr<JsonBuilder> builder = std::make_unique<JsonBuilder>();
+}
+
+TEST(JsonBuilderTest, Full)
+{
+    std::wstring str = L"{\"name\":\"John\", \"age\":11, \"tel\":null, \"FullName\":{ \"firstName\":\"A\", \"lastName\":\"B\", {\"employees\":[{ \"firstName\":\"A\", \"lastName\":\"B\" }, { \"firstName\":\"C\", \"lastName\":\"D\" }] }}";
     std::unique_ptr<JsonBuilder> builder = std::make_unique<JsonBuilder>();
 }
