60d092d47dee9e7ee17455275835fd0895f160bd
David <s1155003185@gmail.com>
2024-06-19 23:57:10 +0800

    Update vpg_object_file_generation_service.hpp

--- include/Service/Generation/vpg_object_file_generation_service.hpp
+++ include/Service/Generation/vpg_object_file_generation_service.hpp
@@ -20,6 +20,7 @@ class VPGObjectFileGenerationService
         // get #include file name
         static std::wstring GetProjectClassIncludeFile(const std::map<std::wstring, std::wstring> &projectClassIncludeFiles, const std::wstring &className);
 
+        // TODO: seperate Hpp and Cpp to generate Json Object
         static void Generate(const LogProperty *logProperty, const std::wstring &classPrefix, const std::map<std::wstring, std::wstring> &projectClassIncludeFiles,
             const std::wstring &filePathHpp, const std::vector<std::shared_ptr<VPGEnumClass>> &enumClassList);
 };
