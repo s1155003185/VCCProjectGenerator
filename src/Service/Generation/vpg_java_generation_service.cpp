86e94be67f4c59320d1490184b9d6c50d0fe4346
David <s1155003185@gmail.com>
2024-08-03 23:09:33 +0800

    fix

--- src/Service/Generation/vpg_java_generation_service.cpp
+++ src/Service/Generation/vpg_java_generation_service.cpp
@@ -13,6 +13,13 @@ using namespace vcc;
 #define JAVA_BRIDGE_FILE_NAME L"DllFunctions.java"
 #define JAVA_PROJECT_SOURCE_PARENT_FOLDER L"src/main/java"
 
+const std::wstring dllExport = L"DLLEXPORT";
+const std::wstring dllExportStart = L"extern \"C\"";
+const std::wstring dllInterfaceExportPropertyAccessor = L"PROPERTY_ACCESSOR_DLL_EXPORT_MACRO_HEADER";
+const std::wstring dllInterfaceExportPropertyAccessorString = L"PROPERTY_ACCESSOR_DLL_EXPORT_MACRO_HEADER_STRING";
+const std::wstring dllInterfaceExportPropertyAccessorObject = L"PROPERTY_ACCESSOR_DLL_EXPORT_MACRO_HEADER_OBJECT";
+const std::wstring dllInterfaceExportPropertyAccessorContainer = L"PROPERTY_ACCESSOR_DLL_EXPORT_MACRO_HEADER_CONTAINER";
+
 std::shared_ptr<VPGGenerationOptionExport> VPGJavaGenerationService::GetJavaOption(const VPGGenerationOption *option)
 {
     std::shared_ptr<VPGGenerationOptionExport> result = nullptr;
@@ -44,8 +51,12 @@ std::wstring VPGJavaGenerationService::GenerateJavaBridgeContent(const std::wstr
 
         std::wstring packageFolder = GetRelativePath(javaOption->GetDllBridgeDirectory(), JAVA_PROJECT_SOURCE_PARENT_FOLDER);
         packageFolder = GetLinuxPath(packageFolder);
+        if (IsStartWith(packageFolder, L"/"))
+            packageFolder = packageFolder.substr(1);
+        if (IsEndWith(packageFolder, L"/"))
+            packageFolder.pop_back();
         ReplaceAll(packageFolder, L"/", L".");
-        result += L"package " + packageFolder + L"\r\n"
+        result += L"package " + packageFolder + L";\r\n"
             "\r\n"
             "import com.sun.jna.Library;\r\n"
             "import com.sun.jna.Native;\r\n"
@@ -55,6 +66,42 @@ std::wstring VPGJavaGenerationService::GenerateJavaBridgeContent(const std::wstr
             + INDENT + filePrefix + L"DllFunctions INSTANCE = (" + filePrefix + L"DllFunctions)Native.load(\"vpg\", " + filePrefix + L"DllFunctions.class);\r\n"
             "\r\n";
 
+        size_t pos = Find(content, dllExportStart);
+        if (pos == std::wstring::npos)
+            THROW_EXCEPTION_MSG(ExceptionType::ParserError, L"DllFunctions.h is missing 'extern \"C\"'");
+
+        pos += dllExportStart.size() - 1;
+        while (pos < content.length()) {
+            std::wstring subStr = content.substr(pos);
+            if (IsStartWith(content, dllExport, pos)) {
+                pos += dllExport.length() - 1;
+
+            } else if (IsStartWith(content, dllInterfaceExportPropertyAccessor, pos)) {
+                // pos = Find(content, L"(", pos);
+                // size_t endPos = Find(content, L")", pos);
+                // pos = endPos;
+            } else if (IsStartWith(content, dllInterfaceExportPropertyAccessorString, pos)) {
+                result += INDENT + L"void ReadString(PointerByReference ref, Integer property, PointerByReference value, Integer index);\r\n"
+                    + INDENT + L"void ReadStringByKey(PointerByReference ref, Integer property, PointerByReference value, PointerByReference key);\r\n"
+                    + INDENT + L"void WriteString(PointerByReference ref, Integer property, PointerByReference value, Integer index);\r\n"
+                    + INDENT + L"void WriteStringByKey(PointerByReference ref, Integer property, PointerByReference value, PointerByReference key);\r\n";
+                //pos += dllInterfaceExportPropertyAccessorString.length() - 1;
+            } else if (IsStartWith(content, dllInterfaceExportPropertyAccessorObject, pos)) {
+                result += INDENT + L"PointerByReference ReadObject(PointerByReference ref, Integer property, Integer index);\r\n"
+                    + INDENT + L"PointerByReference ReadObjectByKey(PointerByReference ref, Integer property, PointerByReference key);\r\n"
+                    + INDENT + L"void WriteObject(PointerByReference ref, Integer property, PointerByReference value, Integer index);\r\n"
+                    + INDENT + L"void WriteObjectByKey(PointerByReference ref, Integer property, PointerByReference value, PointerByReference key);\r\n";
+                //pos += dllInterfaceExportPropertyAccessorObject.length() - 1;
+            } else if (IsStartWith(content, dllInterfaceExportPropertyAccessorContainer, pos)) {
+                result += INDENT + L"Integer GetContainerCount(PointerByReference ref, Integer property);\r\n"
+                    + INDENT + L"Boolean IsContainKey(PointerByReference ref, Integer property, PointerByReference key);\r\n"
+                    + INDENT + L"void RemoveContainerElement(PointerByReference ref, Integer property, Integer index);\r\n"
+                    + INDENT + L"void RemoveContainerElementByKey(PointerByReference ref, Integer property, PointerByReference key);\r\n"
+                    + INDENT + L"void ClearContainer(PointerByReference ref, Integer property);\r\n";
+                //pos += dllInterfaceExportPropertyAccessorContainer.length() - 1;
+            }
+            GetNextCharPos(content, pos, false);
+        }
         result += L"}\r\n";
     CATCH
     return result;
