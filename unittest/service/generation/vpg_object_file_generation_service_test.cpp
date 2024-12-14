77a04d845fc6d5ad7d87d012fabbfa6c5d17ef49
David <s1155003185@gmail.com>
2024-12-14 21:12:37 +0800

    action test

--- unittest/service/generation/vpg_object_file_generation_service_test.cpp
+++ unittest/service/generation/vpg_object_file_generation_service_test.cpp
@@ -698,7 +698,7 @@ TEST_F(VPGObjectFileGenerationServiceTest, InheritForm)
         "// </vcc:customFunctions>\r\n");
 }
 
-TEST_F(VPGObjectFileGenerationServiceTest, FormManagerAndAction)
+TEST_F(VPGObjectFileGenerationServiceTest, FormManager)
 {
     std::wstring enumClass = L""
         "#pragma once\r\n"
@@ -710,8 +710,6 @@ TEST_F(VPGObjectFileGenerationServiceTest, FormManagerAndAction)
         "    , GitManager1 // MANAGER_SPTR(GitManager, GitManager1, _LogProperty)\r\n"
         "    , GitManager2 // MANAGER_SPTR_NULL(GitManager, GitManager2)\r\n"
         "    , GitManager3 // MANAGER_SPTR_PARENT(GitManager, GitManager3, BaseGitManager)\r\n"
-        "    , AddWorkspace // ACTION(AddWorkspace)\r\n"
-        "    , DeleteAction // ACTION(DeleteWorkspace)\r\n"
         "};\r\n"
         "\r\n";
     WriteFile(ConcatPaths({this->_Workspace, L"vcc_object_property.hpp"}), enumClass, true);
@@ -755,8 +753,6 @@ TEST_F(VPGObjectFileGenerationServiceTest, FormManagerAndAction)
         "    MANAGER_SPTR(GitManager, GitManager1, _LogProperty)\r\n"
         "    MANAGER_SPTR_NULL(GitManager, GitManager2)\r\n"
         "    MANAGER_SPTR_PARENT(GitManager, GitManager3, BaseGitManager)\r\n"
-        "    ACTION(AddWorkspace)\r\n"
-        "    ACTION(DeleteWorkspace)\r\n"
         "\r\n"
         "    // <vcc:customVPGGitFormProperties sync=\"RESERVE\" gen=\"RESERVE\">\r\n"
         "    // </vcc:customVPGGitFormProperties>\r\n"
@@ -856,6 +852,131 @@ TEST_F(VPGObjectFileGenerationServiceTest, FormManagerAndAction)
         "        _GitManager1 = std::make_shared<GitManager>(_LogProperty);\r\n"
         "        _GitManager2 = std::make_shared<GitManager>();\r\n"
         "        _GitManager3 = nullptr;\r\n"
+        "        OnInitializeComponents();\r\n"
+        "    CATCH\r\n"
+        "}\r\n"
+        "\r\n"
+        "void VPGGitForm::DoAction(const int64_t &formProperty) const\r\n"
+        "{\r\n"
+        "    TRY\r\n"
+        "        switch(static_cast<VPGGitFormProperty>(formProperty))\r\n"
+        "        {\r\n"
+        "        default:\r\n"
+        "            assert(false);\r\n"
+        "            break;\r\n"
+        "        }\r\n"
+        "    CATCH\r\n"
+        "}\r\n"
+        "\r\n"
+        "// <vcc:customFunctions sync=\"RESERVE\" gen=\"RESERVE\">\r\n"
+        "// </vcc:customFunctions>\r\n");
+}
+
+TEST_F(VPGObjectFileGenerationServiceTest, FormAction)
+{
+    std::wstring enumClass = L""
+        "#pragma once\r\n"
+        "\r\n"
+        "//@@Form\r\n"
+        "enum class VPGGitFormProperty\r\n"
+        "{\r\n"
+        "    AddWorkspace // ACTION(AddWorkspace)\r\n"
+        "    , DeleteAction // ACTION(DeleteWorkspace)\r\n"
+        "};\r\n"
+        "\r\n";
+    WriteFile(ConcatPaths({this->_Workspace, L"vcc_object_property.hpp"}), enumClass, true);
+
+    std::vector<std::shared_ptr<VPGEnumClass>> enumClassList;
+    this->GetReader()->Parse(enumClass, enumClassList);
+
+    std::wstring classPrefix = L"VPG";
+    std::map<std::wstring, std::wstring> projectClassIncludeFiles;
+    VPGObjectFileGenerationService::GenerateHpp(this->GetLogConfig().get(), classPrefix, projectClassIncludeFiles,
+        this->GetFilePathHpp(), this->GetFilePathHpp(), enumClassList);
+    VPGObjectFileGenerationService::GenerateCpp(this->GetLogConfig().get(), classPrefix, projectClassIncludeFiles, this->GetEnumClasses(),
+        this->GetFilePathCpp(), this->GetFilePathCpp(), enumClassList);
+
+    EXPECT_TRUE(IsFileExists(this->GetFilePathHpp()));
+    EXPECT_TRUE(IsFileExists(this->GetFilePathCpp()));
+
+    EXPECT_EQ(ReadFile(this->GetFilePathHpp()), 
+        L"// <vcc:vccproj sync=\"FULL\" gen=\"FULL\"/>\r\n"
+        "#pragma once\r\n"
+        "\r\n"
+        "#include \"base_form.hpp\"\r\n"
+        "#include \"class_macro.hpp\"\r\n"
+        "#include \"object_type.hpp\"\r\n"
+        "\r\n"
+        "// <vcc:customHeader sync=\"RESERVE\" gen=\"RESERVE\">\r\n"
+        "// </vcc:customHeader>\r\n"
+        "\r\n"
+        "using namespace vcc;\r\n"
+        "\r\n"
+        "class VPGGitForm : public BaseForm\r\n"
+        "{\r\n"
+        "    ACTION(AddWorkspace)\r\n"
+        "    ACTION(DeleteWorkspace)\r\n"
+        "\r\n"
+        "    // <vcc:customVPGGitFormProperties sync=\"RESERVE\" gen=\"RESERVE\">\r\n"
+        "    // </vcc:customVPGGitFormProperties>\r\n"
+        "\r\n"
+        "    private:\r\n"
+        "        // <vcc:customVPGGitFormPrivateFunctions sync=\"RESERVE\" gen=\"RESERVE\">\r\n"
+        "        // </vcc:customVPGGitFormPrivateFunctions>\r\n"
+        "\r\n"
+        "    protected:\r\n"
+        "        // <vcc:customVPGGitFormProtectedFunctions sync=\"RESERVE\" gen=\"RESERVE\">\r\n"
+        "        // </vcc:customVPGGitFormProtectedFunctions>\r\n"
+        "\r\n"
+        "    public:\r\n"
+        "        VPGGitForm();\r\n"
+        "        virtual ~VPGGitForm() {}\r\n"
+        "\r\n"
+        "        virtual std::shared_ptr<IObject> Clone() const override;\r\n"
+        "\r\n"
+        "        virtual void InitializeComponents() const override;\r\n"
+        "\r\n"
+        "        virtual void DoAction(const int64_t &formProperty) const override;\r\n"
+        "\r\n"
+        "        // <vcc:customVPGGitFormPublicFunctions sync=\"RESERVE\" gen=\"RESERVE\">\r\n"
+        "        // </vcc:customVPGGitFormPublicFunctions>\r\n"
+        "};\r\n");
+    EXPECT_EQ(ReadFile(this->GetFilePathCpp()),
+        L"// <vcc:vccproj sync=\"FULL\" gen=\"FULL\"/>\r\n"
+        "#include \"vcc_object.hpp\"\r\n"
+        "\r\n"
+        "#include <assert.h>\r\n"
+        "#include <memory>\r\n"
+        "\r\n"
+        "#include \"\"\r\n"
+        "#include \"base_form.hpp\"\r\n"
+        "#include \"exception_macro.hpp\"\r\n"
+        "\r\n"
+        "// <vcc:customHeader sync=\"RESERVE\" gen=\"RESERVE\">\r\n"
+        "// </vcc:customHeader>\r\n"
+        "\r\n"
+        "using namespace vcc;\r\n"
+        "\r\n"
+        "VPGGitForm::VPGGitForm() : BaseForm()\r\n"
+        "{\r\n"
+        "    TRY\r\n"
+        "        _ObjectType = ObjectType::GitForm;\r\n"
+        "        Initialize();\r\n"
+        "    CATCH\r\n"
+        "}\r\n"
+        "\r\n"
+        "std::shared_ptr<IObject> VPGGitForm::Clone() const\r\n"
+        "{\r\n"
+        "    return std::make_shared<VPGGitForm>(*this);\r\n"
+        "}\r\n"
+        "\r\n"
+        "void VPGGitForm::InitializeComponents() const\r\n"
+        "{\r\n"
+        "    TRY\r\n"
+        "        BaseForm::InitializeComponents();\r\n"
+        "        _LogConfig = nullptr;\r\n"
+        "        _ActionManager = nullptr;\r\n"
+        "        _ThreadManager = nullptr;\r\n"
         "        OnInitializeComponents();\r\n"
         "    CATCH\r\n"
         "}\r\n"
