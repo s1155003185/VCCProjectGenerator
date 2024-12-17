46c3866a9cb9620d608eab99ec0c4ccfb12d3822
David <s1155003185@gmail.com>
2024-12-17 10:40:49 +0800

    f

--- include/type/generation/vpg_generation_option_property.hpp
+++ include/type/generation/vpg_generation_option_property.hpp
@@ -49,6 +49,8 @@ enum class VPGGenerationOptionProperty
 
     ApplicationDirectoryHpp, // GETSET(std::wstring, ApplicationDirectoryHpp, L"include")
     ApplicationDirectoryCpp, // GETSET(std::wstring, ApplicationDirectoryCpp, L"src")
+    ActionDirectoryHpp, // GETSET(std::wstring, ActionDirectoryHpp, L"include/action")
+    ActionDirectoryCpp, // GETSET(std::wstring, ActionDirectioryCpp, L"src/action")
     FormDirectoryHpp, // GETSET(std::wstring, FormDirectoryHpp, L"include/form")
     FormDirectoryCpp, // GETSET(std::wstring, FormDirectoryCpp, L"src/form")
     ObjectDirectoryHpp, // GETSET(std::wstring, ObjectDirectoryHpp, L"include/model")
